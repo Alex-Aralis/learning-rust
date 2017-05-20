8e59ab8844426ee93e6a241599309bacce6d35ce
Alex Aralis <alex.aralis@gmail.com>
2017-05-20 15:39:04 -0400

    get basic io working

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,11 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Make a guess!");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("Your guessed {}!", guess);
 }
